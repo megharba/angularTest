76adc50f1a9ffc4826a4ff2fe87f2b36b11516f7
megharba <b.megharba@gmail.com>
2022-05-24 13:58:14 +0200

    exercice 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Statistique } from './models/statistique';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularTest';
+  tabStatistique : Statistique[] = [
+    {
+      id : "fa1f5f40-be3b-11eb-91ec-7f5875ecfb46",
+      titre : "Démographie en France",
+      valeur: "60M",
+    },
+    {
+      id : "2a1r5fo0-be3b-11eb-91ec-75875ectfb46",
+      titre : "Démographie en Espagne",
+      valeur: "50M",
+    }
+  ];
 }
